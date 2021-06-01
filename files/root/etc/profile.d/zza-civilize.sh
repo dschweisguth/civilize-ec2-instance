7666aa013f49620f5aab83d118542d2b95ad1c62
Dave Schweisguth <dave@schweisguth.org>
2021-06-01 06:19:08 -0700

    Set prompt. Add a couple of aliases.

--- files/root/etc/profile.d/zza-civilize.sh
+++ files/root/etc/profile.d/zza-civilize.sh
@@ -5,6 +5,11 @@ fi
 export EDITOR=emacs
 export LESS=-FX
 
+unset PROMPT_COMMAND
+if [ "$PS1" ]; then
+  PS1='\[\e[1m\][\u \w]\[\e[m\] '
+fi
+
 unalias egrep
 unalias fgrep
 unalias grep
@@ -12,6 +17,8 @@ unalias l.
 unalias ll
 unalias which
 
+alias b=bundle
+alias be="bundle exec"
 alias e=emacs
 alias ls="ls -p"
 alias m=more
